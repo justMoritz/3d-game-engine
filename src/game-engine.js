243564db10c7704108a2be856f126605ac54af1e
MMC Moritz Laptop <mmcmoritzlaptop@mmc-moritz-macbook.local>
2020-10-21 21:33:57 -0500

    cleaned up code and syntax

--- src/game-engine.js
+++ src/game-engine.js
@@ -8,16 +8,16 @@ var gameEngineJS = (function(){
 
   var fPlayerX = 8.0;
   var fPlayerY = 8.0;
-  var fPlayerA = 0.0;
+  var fPlayerA = 1.15;
 
   var nMapHeight = 16;
   var nMapWidth = 16;
 
-  var fFOV = 3.14159 / 3.0;
+  var fFOV = 3.14159 / 4.0;
   var fDepth = 16.0; // viewport depth
 
   var map = "";
-  map += "################";
+  map += "#..............#";
   map += "#..............#";
   map += "#..............#";
   map += "#..........#...#";
@@ -26,7 +26,7 @@ var gameEngineJS = (function(){
   map += "#..............#";
   map += "#..............#";
   map += "#..............#";
-  map += "#..............#";
+  map += "###...##..##...#";
   map += "#..............#";
   map += "#..............#";
   map += "#..............#";
@@ -43,13 +43,17 @@ var gameEngineJS = (function(){
   };
 
 
-
+  /**
+   * Renderer
+   */
   var _fDrawFrame = function(oInput)
   {
     var sOutput = '';
+
     // loops through each pixel and appends it to the output
     for(var i = 0; i < oInput.length; i++){
-      // H blank based on screen-width
+
+      // insert H blank based on screen-width
       if(i % nScreenWidth == 0){
         sOutput += '<br>';
       }
@@ -64,8 +68,7 @@ var gameEngineJS = (function(){
    */
   var main = function()
   {
-    // setInterval(gameLoop, 33);
-    setInterval(gameLoop, 66);
+    setInterval(gameLoop, 33);
     function gameLoop(){
 
       var screen = [];
@@ -75,7 +78,7 @@ var gameEngineJS = (function(){
 
         // calculates the ray angle into the world space
         // take the current player angle, subtract half the field of view
-        // and then chopping it up into little bits of the screen width (at the current colum)
+        // and then chop it up into equal little bits of the screen width (at the current colum)
         var fRayAngle = (fPlayerA - fFOV / 2) + (i / nScreenWidth) * fFOV;
 
         var fDistanceToWall = 0;
@@ -96,61 +99,67 @@ var gameEngineJS = (function(){
             // TODO: Figure out why this didn't work
             // fDistanceToWall = fDepth;
           }else{
-            // Ray is in the bounds, so let's see if the ray cell wall is a block, however it works.
-            // converts our position in 3D space into 2D (because the map is stored in a 2D array)
-            if(map[nTestY * nMapWidth + nTestX == '#']){
+            // Ray is in the bounds, so let's see if the ray cell wall is a block.
+            // converts our position in 3D space into the 2D coordinates
+            // (because the map is stored in a 2D array)
+            if(map[nTestY * nMapWidth + nTestX] == '#'){
               bHitWall = true;
 
               // Exp: fDistanceToWall will retain it's distance value;
+              // It was incremented above.
             }
           }
         } // end angle loop
 
 
         // based on the distance to wall, determine how much floor and ceiling to show per column,
-        // or, to put in another way, how big or small to paint the rendered wall
+        // or, to put in another way, how big or small to paint the rendered wall per column
         var nCeiling = (nScreenHeight / 2.0) - nScreenHeight / fDistanceToWall;
         var nFloor = nScreenHeight - nCeiling;
-
-        // &block;
-        // &blk34;
-        // &blk12;
-        // &blk14;
-
-        var nShade = '&nbsp;';
 
 
         // draw the column, one screenheight pixel at a time
         for(var j = 0; j < nScreenHeight; j++){
           if( j < nCeiling){
+
             // draw ceiling/sky
             screen[j*nScreenWidth+i] = '&nbsp;';
           }
           else if( j > nCeiling && j <= nFloor ){
-            // draw wall
 
-
+            // draw wall, in different shades
             if(fDistanceToWall < fDepth / 4){
               screen[j*nScreenWidth+i] = '&block;';
-            }else if(fDistanceToWall < fDepth / 3){
+            }
+            else if(fDistanceToWall < fDepth / 3){
               screen[j*nScreenWidth+i] = '&blk34;';
-            }else if(fDistanceToWall < fDepth / 2){
+            }
+            else if(fDistanceToWall < fDepth / 2){
               screen[j*nScreenWidth+i] = '&blk12;';
-            }else{
+            }
+            else{
               screen[j*nScreenWidth+i] = '&blk14;';
             }
-
           }
           else{
-            // draw floor
-            screen[j*nScreenWidth+i] = '.';
+
+            // draw floor, in different shades
+            b = 1 - (j -nScreenHeight / 2) / (nScreenHeight / 2);
+            if(b < 0.25){
+              screen[j*nScreenWidth+i] = 'x';
+            }else if(b < 0.5){
+              screen[j*nScreenWidth+i] = '=';
+            }else if(b < 0.75){
+              screen[j*nScreenWidth+i] = '-';
+            }else if(b < 0.9){
+              screen[j*nScreenWidth+i] = '.';
+            }else{
+              screen[j*nScreenWidth+i] = ' ';
+            }
           }
-        }
-
-
+        } // end draw column loop
 
       }  // end column loop
-
 
       _fDrawFrame(screen);
 
@@ -185,27 +194,61 @@ var gameEngineJS = (function(){
 
       if(e.keyCode == 97){
         // left arrow (a)
-        fPlayerA -= 0.2;
+        fPlayerA -= 0.15;
       }
       else if(e.keyCode == 100){
         // right arrow (d)
-        fPlayerA += 0.2;
+        fPlayerA += 0.15;
       }
       else if(e.keyCode == 119){
         // forward arrow (w)
-        fPlayerX += Math.sin(fPlayerA) + 0.2;
-        fPlayerY += Math.cos(fPlayerA) + 0.2;
+        fPlayerX += Math.sin(fPlayerA) + 5.0 * 0.0051;
+        fPlayerY += Math.cos(fPlayerA) + 5.0 * 0.0051;
       }
       else if(e.keyCode == 115){
         // backward arrow (s)
-        fPlayerX -= Math.sin(fPlayerA) + 0.2;
-        fPlayerY -= Math.cos(fPlayerA) + 0.2;
+        fPlayerX -= Math.sin(fPlayerA) + 5.0 * 0.0051;
+        fPlayerY -= Math.cos(fPlayerA) + 5.0 * 0.0051;
       }
     };
 
 
 
-    // _testCoordinates();
+    // // mousemove logig
+
+    // var oldX = 0, oldY = 0;
+    // function captureMouseMove($event){
+
+    //   var directionX = 0, directionY = 0, diffX = 0, diffY = 0;
+    //   if ($event.pageX < oldX) {
+    //       directionX = "left"
+    //       diffX = oldX - $event.pageX;
+
+    //       fPlayerA -= diffX * 0.0005;
+    //   } else if ($event.pageX > oldX) {
+    //       directionX = "right"
+    //       diffX = $event.pageX - oldX;
+
+    //       fPlayerA += diffX * 0.0005;
+    //   }
+
+    //   if ($event.pageY < oldY) {
+    //       directionY = "top"
+    //       diffY = oldY - $event.pageY;
+    //       // fPlayerX += Math.sin(fPlayerA) + 5.0 * 0.000051;
+    //       // fPlayerY += Math.cos(fPlayerA) + 5.0 * 0.000051;
+    //   } else if ($event.pageY > oldY) {
+    //       directionY = "bottom";
+    //       diffY = $event.pageY - oldY;
+    //       // fPlayerX -= Math.sin(fPlayerA) + 5.0 * 0.000051;
+    //       // fPlayerY -= Math.cos(fPlayerA) + 5.0 * 0.000051;
+    //   }
+
+    //   oldX = $event.pageX;
+    //   oldY = $event.pageY;
+    // }
+
+    window.addEventListener('mousemove', captureMouseMove);
   };
 
 
@@ -213,14 +256,3 @@ var gameEngineJS = (function(){
     init: init,
   }
 })();
-
-
-// ░
-// ▒
-// ▓
-// █ &blk34;
-
-
-// ▄
-// ■
-// ▀
