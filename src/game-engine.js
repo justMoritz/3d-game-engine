77bb7b9fb5b23d0af55ae792b4aba25340a29413
Moritz <mz1440@Moritzs-MBP.lan>
2024-01-09 20:25:54 -0600

    added some color values

--- src/game-engine.js
+++ src/game-engine.js
@@ -475,10 +475,10 @@ var gameEngineJS = (function () {
     // Convert values to shades of grey
     for (var i = 0; i < pixels.length; i++) {
       var pixelValue = pixels[i];
-      var greyShade = pixelValue * 63; // Adjust the factor based on your preference
-      imageData.data[i * 4] = greyShade; // Red channel
-      imageData.data[i * 4 + 1] = greyShade/2; // Green channel
-      imageData.data[i * 4 + 2] = greyShade/4; // Blue channel
+      var color = _rh.pixelLookupTable[pixelValue] || [0, 0, 0]; // Default to black if not found
+      imageData.data[i * 4] = color[0]; // Red channel
+      imageData.data[i * 4 + 1] = color[1]; // Green channel
+      imageData.data[i * 4 + 2] = color[2]; // Blue channel
       imageData.data[i * 4 + 3] = 255; // Alpha channel (fully opaque)
     }
     // Use putImageData to draw the pixels onto the canvas
@@ -530,53 +530,99 @@ var gameEngineJS = (function () {
   // each texture has 4 values: 3 hues plus black
   // each value can be rendered with 5 shades (4 plus black)
   var _rh = {
+    pixelLookupTable: {
+      0: [0, 0, 0], // Black
+      1: [66, 66, 66], // 4 Grey Values
+      2: [133, 133, 133],
+      3: [200, 200, 200],
+      4: [255, 255, 255], // White
+      a: [32, 24, 136], // 4 Blues
+      b: [32, 56, 232],
+      c: [88, 144, 248],
+      d: [192, 208, 248],
+      e: [136, 0, 112], // 4 pinks
+      f: [184, 0, 184],
+      g: [240, 120, 248],
+      h: [248, 192, 248],
+      i: [160, 0, 0], // 4 reds
+      j: [216, 40, 66],
+      k: [248, 112, 96],
+      l: [248, 184, 176],
+      m: [114, 64, 7], // 4 oranges
+      n: [136, 112, 0],
+      o: [199, 178, 28],
+      p: [220, 206, 112],
+      q: [0, 80, 0], // 4 greens
+      r: [0, 168, 0], 
+      s: [72, 216, 72],
+      t: [168, 240, 184],
+      u: [24, 56, 88], // 4 teals
+      v: [0, 128, 136],
+      w: [0, 232, 217],
+      x: [152, 248, 240],
+      // Add more entries as needed
+    },
     renderWall: function (j, fDistanceToWall, sWallDirection, pixel) {
       var fill = "";
 
-      var b100 = "4";
-      var b75 = "3";
-      var b50 = "2";
-      var b25 = "1";
+      // TODO: 
+      // Maybe can pass the color-family for the pixel here, 
+      // and then change the bxx variables accordingly
+
       var b0 = "0";
+      var b20 = "u";
+      var b40 = "v";
+      var b80 = "w";
+      var b60 = "x";
+
+      var b100 = "4";
 
       if (sWallDirection === "N" || sWallDirection === "S") {
         if (fDistanceToWall < fDepth / 5.5) {
           if (pixel === "#") {
             fill = b100;
           } else if (pixel === "7") {
-            fill = b75;
-          } else if (pixel === "*" || pixel === "o") {
-            fill = b50;
+            fill = b80;
+          } else if (pixel === "*" ) {
+            fill = b60;
+          } else if (pixel === "o") {
+            fill = b40;
           } else {
-            fill = b25;
+            fill = b20;
           }
         } else if (fDistanceToWall < fDepth / 3.66) {
           if (pixel === "#") {
-            fill = b75;
+            fill = b80;
           } else if (pixel === "7") {
-            fill = b50;
-          } else if (pixel === "*" || pixel === "o") {
-            fill = b25;
+            fill = b60;
+          } else if (pixel === "*" ) {
+            fill = b40;
+          } else if ( pixel === "o") {
+            fill = b20;
           } else {
             fill = b0;
           }
         } else if (fDistanceToWall < fDepth / 2.33) {
           if (pixel === "#") {
-            fill = b50;
+            fill = b60;
           } else if (pixel === "7") {
-            fill = b25;
-          } else if (pixel === "*" || pixel === "o") {
-            fill = b25;
+            fill = b40;
+          } else if (pixel === "*" ) {
+            fill = b20;
+          } else if (pixel === "o") {
+            fill = b20;
           } else {
             fill = b0;
           }
         } else if (fDistanceToWall < fDepth / 1) {
           if (pixel === "#") {
-            fill = b25;
+            fill = b40;
           } else if (pixel === "7") {
-            fill = b25;
-          } else if (pixel === "*" || pixel === "o") {
-            fill = b25;
+            fill = b20;
+          } else if (pixel === "*") {
+            fill = b20;
+          } else if ( pixel === "o") {
+            fill = b20;
           } else {
             fill = b0;
           }
@@ -589,40 +635,48 @@ var gameEngineJS = (function () {
       else {
         if (fDistanceToWall < fDepth / 5.5) {
           if (pixel === "#") {
-            fill = b75;
+            fill = b80;
           } else if (pixel === "7") {
-            fill = b50;
-          } else if (pixel === "*" || pixel === "o") {
-            fill = b25;
+            fill = b40;
+          } else if (pixel === "*" ) {
+            fill = b20;
+          } else if (pixel === "o") {
+            fill = b20;
           } else {
             fill = b0;
           }
         } else if (fDistanceToWall < fDepth / 3.66) {
           if (pixel === "#") {
-            fill = b50;
+            fill = b60;
           } else if (pixel === "7") {
-            fill = b50;
-          } else if (pixel === "*" || pixel === "o") {
-            fill = b25;
+            fill = b60;
+          } else if (pixel === "*") {
+            fill = b40;
+          } else if (pixel === "o") {
+            fill = b20;
           } else {
             fill = b0;
           }
         } else if (fDistanceToWall < fDepth / 2.33) {
           if (pixel === "#") {
-            fill = b50;
+            fill = b60;
           } else if (pixel === "7") {
-            fill = b25;
-          } else if (pixel === "*" || pixel === "o") {
-            fill = b25;
+            fill = b40;
+          } else if (pixel === "*") {
+            fill = b40;
+          } else if (pixel === "o") {
+            fill = b20;
           } else {
             fill = b0;
           }
         } else if (fDistanceToWall < fDepth / 1) {
           if (pixel === "#") {
-            fill = b25;
+            fill = b40;
           } else if (pixel === "7") {
-            fill = b25;
-          } else if (pixel === "*" || pixel === "o") {
+            fill = b40;
+          } else if (pixel === "*") {
+            fill = b20;
+          } else if (pixel === "o") {
             fill = b0;
           } else {
             fill = b0;
@@ -690,10 +744,12 @@ var gameEngineJS = (function () {
     renderFloor: function (j) {
       var fill = "`";
 
+      
+      // b = 1 - 
+      //   (j - nScreenHeight / 2) / (nScreenHeight / 2);
+
       // draw floor, in different shades
-      b = 1 - (j - nScreenHeight / 2) / (nScreenHeight / 2);
-      b =
-        1 -
+      b = 1 -
         (j - nScreenHeight / (2 - fLooktimer * 0.15)) /
           (nScreenHeight / (2 - fLooktimer * 0.15));
 
