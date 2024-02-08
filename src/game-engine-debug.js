a73c41f06c266000393c127733cd84b1c2296d1d
Moritz Zimmer <moritz.zimmer@sabre.com>
2024-02-08 10:34:35 -0600

    simplofied game loop

--- src/game-engine-debug.js
+++ src/game-engine-debug.js
@@ -15,9 +15,9 @@ testmap = {
   nMapHeight: 7,
   nMapWidth: 7,
   map: map,
-  fPlayerX: 2.7,
-  fPlayerY: 2.8,
-  fPlayerA: 1.2,
+  fPlayerX: 2.7195467142900243,
+  fPlayerY: 2.8010949729988464,
+  fPlayerA: 1.25,
   fDepth: 9,
 };
 
@@ -28,62 +28,7 @@ function approximatelyEqual(a, b, epsilon) {
 
 
 var gameEngineJS = (function () {
-  // constants
-  var PI___ = +Math.PI;
-  var PI_0 = 0.0;
-  var PIx0_25 = +(PI___ * 0.25);
-  var PIx05 = +(PI___ * 0.5);
-  var PIx0_75 = +(PI___ * 0.75);
-  var PIx1 = PI___;
-  var PIx1_5 = +(PI___ * 1.5);
-  var PIx2 = +(PI___ * 2.0);
-  var I80divPI = 180 / PI___;
-  var PIdiv4 = PI___ / 4.0;
-
-  // setup variables
-  var eScreen;
-  var eCanvas;
-  var cCtx;
-  var eDebugOut;
-
-  var nScreenWidth = 420;
-  var nScreenHeight = 120;
-
-  var fFOV = PI___ / 1.8; // (PI___ / 4.0 originally)
-  var fDepth = 16.0; // viewport depth
-  var nLookLimit = 8;
-
-  var bTurnLeft;
-  var bTurnRight;
-  var bStrafeLeft;
-  var bStrafeRight;
-  var bMoveForward;
-  var bMoveBackward;
-  var bRunning;
-  var bPaused;
-  var bPlayerMayMoveForward = true;
-
-  var nJumptimer = 0;
-  var fLooktimer = 0;
-
-  var fDepthBuffer = [];
-
-  // defaults
-  var fPlayerX = 14.0;
-  var fPlayerY = 1.0;
-  var fPlayerA = 1.5;
-
-  var nMapHeight = 16;
-  var nMapWidth = 16;
-
-  var gameRun;
-
-
-  var _randomIntFromInterval = function (min, max) {
-    // min and max included
-    return ~~(Math.random() * (max - min + 1) + min);
-  };
-
+  
 
   /**
    * Loads
@@ -134,6 +79,7 @@ var gameEngineJS = (function () {
   }
 
   var _fDrawFrame = function (screen, target) {
+    _debugOutput(`A: ${fPlayerA} X:${fPlayerX} Y:${fPlayerY}`)
     var frame = screen
     var target = target || eScreen;
 
@@ -452,6 +398,56 @@ var gameEngineJS = (function () {
     },
   };
 
+  // constants
+  var PI___ = +Math.PI;
+  var PI_0 = 0.0;
+  var PIx0_25 = +(PI___ * 0.25);
+  var PIx05 = +(PI___ * 0.5);
+  var PIx0_75 = +(PI___ * 0.75);
+  var PIx1 = PI___;
+  var PIx1_5 = +(PI___ * 1.5);
+  var PIx2 = +(PI___ * 2.0);
+  var I80divPI = 180 / PI___;
+  var PIdiv4 = PI___ / 4.0;
+
+  // setup variables
+  var eScreen;
+  var eCanvas;
+  var cCtx;
+  var eDebugOut;
+
+  var nScreenWidth = 420;
+  var nScreenHeight = 120;
+
+  var fFOV = PI___ / 1.8; // (PI___ / 4.0 originally)
+  var fDepth = 16.0; // viewport depth
+  var nLookLimit = 8;
+
+  var bTurnLeft;
+  var bTurnRight;
+  var bStrafeLeft;
+  var bStrafeRight;
+  var bMoveForward;
+  var bMoveBackward;
+  var bRunning;
+  var bPaused;
+  var bPlayerMayMoveForward = true;
+
+  var nJumptimer = 0;
+  var fLooktimer = 0;
+
+  var fDepthBuffer = [];
+
+  // defaults
+  var fPlayerX = 14.0;
+  var fPlayerY = 1.0;
+  var fPlayerA = 1.5;
+
+  var nMapHeight = 16;
+  var nMapWidth = 16;
+
+  var gameRun;
+
   /**
    * The basic game loop
    */
@@ -579,15 +575,14 @@ var gameEngineJS = (function () {
               sWallDirection = "E";
             }
 
-            if(i > 60 && i < 70){
+            if(i > 50 && i < 60){
               console.log(`${sWallDirection}  ${i} : Angle: ${fTestAngle} Block: ${sWallDirection}/${sWalltype}`);
             }
-            if(i == 70){
+            if(i == 60){
               console.log('---')
             }
           } 
           // END TEST FOR WALLS
-
         
         } /** End Ray Casting Loop **/
 
